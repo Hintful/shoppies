939fb79c6359a54becfaffd103f7c8bebb316af3
Kurt Choi <choi.kurt3@gmail.com>
2021-05-04 22:07:33 -0400

    Debounce input setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { DebounceInput } from 'react-debounce-input';
+import { useState } from 'react';
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
   return (
     <div className="App">
-
+      <DebounceInput minLength={3} debounceTimeout={500} onChange = {e => setSearchQuery(e.target.value)} />
+      { searchQuery }
     </div>
   );
 }
