7d927b915a48d38758e7e08653697b21e8735942
Kurt Choi <choi.kurt3@gmail.com>
2021-05-06 00:00:50 -0400

    Add information with tooltip element for search bar

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { DebounceInput } from 'react-debounce-input';
+import ReactTooltip from 'react-tooltip';
 
 const SearchBox = ({ setSearchQuery }) => {
   return (
@@ -9,7 +10,11 @@ const SearchBox = ({ setSearchQuery }) => {
         <DebounceInput className="search-bar-input" minLength={3} debounceTimeout={500} onChange={e => setSearchQuery(e.target.value)} 
           placeholder="Search by title"
         />
+        <i className="fas fa-info-circle info-icon" style={{ color: "#bdbdbd", fontSize: "20px" }} 
+          data-tip="Top 50 results will show up. Please search more specifically if the movie you're looking for is not showing up."
+        />
       </div>
+      <ReactTooltip effect="solid"/>
     </div>
   );
 }
